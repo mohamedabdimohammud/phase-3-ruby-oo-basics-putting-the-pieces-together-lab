f3a4eb53dc31953dd1a26f0336fbdc2a0d3a3597
mohamedabdimohammud <mmohammud@outlook.com>
2023-05-22 21:51:11 +0300

    Completed assignment

--- spec/01_book_spec.rb
+++ spec/01_book_spec.rb
@@ -36,3 +36,52 @@ describe 'Book' do
     end
   end
 end
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+describe 'Book' do
+  describe '.new' do
+    it 'gets initialized with a title' do
+      expect { Book.new("And Then There Were None") }.not_to raise_error
+    end
+  end
+
+  describe 'properties' do
+    let(:book) { Book.new("And Then There Were None") }
+
+    it 'has a title' do
+      expect(book.title).to eq("And Then There Were None")
+    end
+
+    it 'has an author name' do
+      book.author = "Agatha Christie"
+      expect(book.author).to eq("Agatha Christie")
+    end
+
+    it 'has a page count' do
+      book.page_count = 272
+      expect(book.page_count).to eq(272)
+    end
+
+    it 'has a genre' do
+      book.genre = "Mystery"
+      expect(book.genre).to eq("Mystery")
+    end
+  end
+
+  describe '#turn_page' do
+    it 'can turn the page' do
+      book = Book.new("The World According to Garp")
+      expect { book.turn_page }.to output(a_string_matching("Flipping the page...wow, you read fast!")).to_stdout
+    end
+  end
+end
--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -49,3 +49,65 @@ describe 'Shoe' do
     end
   end
 end
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+describe 'Shoe' do
+  describe '.new' do
+    it 'gets initialized with a brand' do
+      expect { Shoe.new("Adidas") }.not_to raise_error
+    end
+  end
+
+  describe 'properties' do
+    let(:shoe) { Shoe.new("Nike") }
+
+    it 'has a brand' do
+      expect(shoe.brand).to eq("Nike")
+    end
+
+    it 'has a color' do
+      shoe.color = "red"
+      expect(shoe.color).to eq("red")
+    end
+
+    it 'has a size' do
+      shoe.size = 9.5
+      expect(shoe.size).to eq(9.5)
+    end
+
+    it 'has a material' do
+      shoe.material = "suede"
+      expect(shoe.material).to eq("suede")
+    end
+
+    it 'has a condition' do
+      shoe.condition = "tattered"
+      expect(shoe.condition).to eq("tattered")
+    end
+  end
+
+  describe '#cobble' do
+    let(:shoe) { Shoe.new("Nike") }
+
+    it 'says that the shoe has been repaired' do
+      expect { shoe.cobble }.to output(a_string_matching("Your shoe is as good as new!")).to_stdout
+    end
+
+    it 'makes the shoe\'s condition new' do
+      shoe.condition = "old"
+      shoe.cobble
+      expect(shoe.condition).to eq("new")
+    end
+  end
+end
